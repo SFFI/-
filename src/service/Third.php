75c49636ca28b2479abe6cca325e0c147b6f9ecd
caoganghui <caoganghui@cmrpt.com>
2020-08-06 11:36:29 +0800

    '添加新增用户'

--- src/service/Third.php
+++ src/service/Third.php
@@ -10,6 +10,7 @@ use sffi\util\RequestUtil;
 /**
  * Class Member
  * @method array query($unionid,$type='',$open_id='') 查询第三方信息
+ * @method array create($unionid,$openid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='') 新增第三方信息
  * @package sffi\service
  */
 class Third extends Base
@@ -17,7 +18,18 @@ class Third extends Base
     protected $argQuery = [
         'unionid',
         'type',
-        'open_id'
+        'openid'
+    ];
+
+    protected $argCreate = [
+        'openid',
+        'unionid',
+        'type',
+        'nickname',
+        'head_img_url',
+        'province',
+        'city',
+        'country'
     ];
 
 }
