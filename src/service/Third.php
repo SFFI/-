b01ec19e7b453b9ecd1a59d4cd8d8571783cf699
黄连木笛 <leihuahui@outlook.com>
2021-03-09 17:48:20 +0800

    Third

--- src/service/Third.php
+++ src/service/Third.php
@@ -9,7 +9,7 @@ use sffi\util\RequestUtil;
 
 /**
  * Class Member
- * @method array query($unionid='',$type='',$open_id='') 查询第三方信息
+ * @method array query($unionid='',$type='',$open_id='', $member='') 查询第三方信息
  * @method array create($unionid,$openid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='') 新增第三方信息
  * @package sffi\service
  */
@@ -18,7 +18,8 @@ class Third extends Base
     protected $argQuery = [
         'unionid',
         'type',
-        'openid'
+        'openid',
+        'member'
     ];
 
     protected $argCreate = [
