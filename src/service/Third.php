d1ab3f043e1b5cd5c061bace1f04457e52360680
caoganghui <caoganghui@cmrpt.com>
2021-04-17 16:11:17 +0800

    创建第三方数据新增两个字段

--- src/service/Third.php
+++ src/service/Third.php
@@ -10,7 +10,7 @@ use sffi\util\RequestUtil;
 /**
  * Class Member
  * @method array query($unionid='',$type='',$open_id='', $member='') 查询第三方信息
- * @method array create($openid,$unionid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='') 新增第三方信息
+ * @method array create($openid,$unionid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='',$session_key='',$session_key_time='') 新增第三方信息
  * @method array bindMember($unionid,$openid,$member_id,$type) 绑定用户
  * @method array unBindMember($unionid,$openid,$member_id,$type) 解绑用户
  * @package sffi\service
@@ -32,7 +32,9 @@ class Third extends Base
         'head_img_url',
         'province',
         'city',
-        'country'
+        'country',
+        'session_key',
+        'session_key_time'
     ];
 
     protected $argBindMember = [
