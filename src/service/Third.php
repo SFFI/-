0798bdeb197fb0f3e93ecbadb8c390956a0a9589
黄连木笛 <leihuahui@outlook.com>
2021-04-30 11:48:28 +0800

    Third

--- src/service/Third.php
+++ src/service/Third.php
@@ -11,6 +11,7 @@ use sffi\util\RequestUtil;
  * Class Member
  * @method array query($unionid='',$type='',$open_id='', $member='') 查询第三方信息
  * @method array create($openid,$unionid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='',$session_key='',$session_key_time='') 新增第三方信息
+ * @method array update($id,$data) 编辑第三方信息
  * @method array bindMember($unionid,$openid,$member_id,$type) 绑定用户
  * @method array unBindMember($unionid,$openid,$member_id,$type) 解绑用户
  * @package sffi\service
@@ -35,6 +36,11 @@ class Third extends Base
         'country',
         'session_key',
         'session_key_time'
+    ];
+
+    protected $argUpdate = [
+        'id',
+        'data'
     ];
 
     protected $argBindMember = [
