e75f422d99597b20a74b04c991c6a3667bd3d1dd
caoganghui <caoganghui@cmrpt.com>
2021-12-10 17:08:13 +0800

    platform app

--- src/service/PlatformApp.php
+++ src/service/PlatformApp.php
@@ -5,7 +5,7 @@ namespace sffi\service;
 /**
  * 平台app
  * Class PlatformApp
- * @method array create($name,$app_type_code) 新增app
+ * @method array create($name,$app_type_code,$app_id='') 新增app $app_type_code来源用户中心app类型 $app_id为空则由用户中心生成
  * @method array list() app列表
  * @method array delete($id) 删除app
  * @method array app_type() 获取app类型
@@ -15,7 +15,8 @@ class PlatformApp extends Base
 {
     protected $argCreate = [
         'name',
-        'app_type_code'
+        'app_type_code',
+        'app_id'
     ];
 
     protected $argList = [];
