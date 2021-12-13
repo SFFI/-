c80aef2220bf07e0a7089e7368be40ced38f1f60
caoganghui <caoganghui@cmrpt.com>
2021-12-13 14:47:26 +0800

    singleQuery

--- src/service/Third.php
+++ src/service/Third.php
@@ -1,15 +1,11 @@
 <?php
 
-
 namespace sffi\service;
-
-
-use sffi\util\RequestUtil;
-
 
 /**
  * Class Member
  * @method array query($unionid='',$type='',$open_id='', $member='') 查询第三方信息
+ * @method array singleQuery($member_id) 查询第三方信息
  * @method array create($openid,$unionid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='',$session_key='',$session_key_time='') 新增第三方信息
  * @method array update($id,$data) 编辑第三方信息
  * @method array bindMember($unionid,$openid,$member_id,$type) 绑定用户
@@ -18,6 +14,10 @@ use sffi\util\RequestUtil;
  */
 class Third extends Base
 {
+    protected $argSingleQuery = [
+        'member_id'
+    ];
+
     protected $argQuery = [
         'unionid',
         'type',
