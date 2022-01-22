fbb8dcffe5f4e198c5227ecac5c6f624e6d8ef58
NG <yuyi@cmrpt.com>
2022-01-22 17:43:19 +0800

    完善队列下单，兼容多种类型

--- src/service/Third.php
+++ src/service/Third.php
@@ -10,6 +10,7 @@ use sffi\util\RequestUtil;
 /**
  * Class Member
  * @method array query($unionid='',$type='',$open_id='', $member='') 查询第三方信息
+ * @method array queryMulti($open_id='', $member='') 查询第三方信息
  * @method array create($openid,$unionid='',$type='',$nickname='',$head_img_url='',$province='',$city='',$country='',$session_key='',$session_key_time='') 新增第三方信息
  * @method array update($id,$data) 编辑第三方信息
  * @method array bindMember($unionid,$openid,$member_id,$type) 绑定用户
@@ -57,4 +58,8 @@ class Third extends Base
         'type'
     ];
 
+    protected $argQueryMulti = [
+        'openid',
+        'member'
+    ];
 }
